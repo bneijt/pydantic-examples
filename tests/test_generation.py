a0768b58c9cca9f69e55de424851b4df73a903be
Bram Neijt <bram@neijt.nl>
2024-10-11 21:03:33 +0200

    Add support for list/dict

--- tests/test_generation.py
+++ tests/test_generation.py
@@ -18,25 +18,3 @@ def test_should_include_docstring() -> None:
 value: foo  # Does not really matter
 """
     )
-
-def test_should_work_with_nesting() -> None:
-    class Nested(BaseModel):
-        """Nested type"""
-        value: Annotated[str, Field(description="Nested value")] = "also foo"
-    
-    class Example(BaseModel):
-        """Example model"""
-
-        normal: Annotated[str, Field(description="Does not really matter")] = "foo"
-        nested_value: Nested = Nested()
-
-    a = yaml_with_comments(Example())
-    assert (
-        a
-        == """# Example model
-normal: foo  # Does not really matter
-nested_value:
-# Nested type
-  value: also foo  # Nested value
-"""
-    )
